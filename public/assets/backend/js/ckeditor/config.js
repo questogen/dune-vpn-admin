281bb3f9a8169a777905da6e067e699dd3e645b8
questogen <89968346+questogen@users.noreply.github.com>
2025-04-21 21:46:50 +0300

    Add files via upload

---
+++ public/assets/backend/js/ckeditor/config.js
@@ -0,0 +1,7 @@
+CKEDITOR.editorConfig = function (config) {
+    config.extraPlugins = ["youtube", "base64image"];
+};
+
+CKEDITOR.editorConfig = function (config) {
+    config.versionCheck = false;
+};
