94b1482597109d1fdedd75f970a15e35d70f32bc
questogen <89968346+questogen@users.noreply.github.com>
2025-04-21 19:54:02 +0300

    Create Admin.php

---
+++ app/Models/Admin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
+
+class Admin extends Authenticatable
+{
+    use HasFactory, Notifiable, HasRoles;
+
+    protected $fillable = ['username', 'name', 'email', 'status', 'password',];
+    
+    protected $hidden = ['password', 'remember_token',];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
